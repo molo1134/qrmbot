eb1aecab79e9c329e9361cb286da6798ecb578a4
molo1134 <molo1134@gmx.com>
2018-07-29 11:31:50 -0400

    debug logging

--- scripts/isregistered.tcl
+++ scripts/isregistered.tcl
@@ -17,14 +17,14 @@ proc who_nickchange {nick uhost hand chan newnick} {
 proc rpl_whoreply {from cmd text} {
 	putlog "whoreply called; from: $from cmd: $cmd text: $text"
 	set repl [split $text " "]
-	putlog "reply 0: " [lindex $repl 0]
-	putlog "reply 1: " [lindex $repl 1]
-	putlog "reply 2: " [lindex $repl 2]
-	putlog "reply 3: " [lindex $repl 3]
-	putlog "reply 4: " [lindex $repl 4]
-	putlog "reply 5: " [lindex $repl 5]
-	putlog "reply 6: " [lindex $repl 6]
-	putlog "reply 7: " [lindex $repl 7]
-	putlog "reply 8: " [lindex $repl 8]
+	putlog [append "reply 0: " [lindex $repl 0]]
+	putlog [append "reply 1: " [lindex $repl 1]]
+	putlog [append "reply 2: " [lindex $repl 2]]
+	putlog [append "reply 3: " [lindex $repl 3]]
+	putlog [append "reply 4: " [lindex $repl 4]]
+	putlog [append "reply 5: " [lindex $repl 5]]
+	putlog [append "reply 6: " [lindex $repl 6]]
+	putlog [append "reply 7: " [lindex $repl 7]]
+	putlog [append "reply 8: " [lindex $repl 8]]
 }
 
