58be1a6c5a8015b6d1301243fb92029bcfe00b81
molo1134 <molo1134@gmx.com>
2018-07-29 11:24:45 -0400

    debug logging

--- scripts/isregistered.tcl
+++ scripts/isregistered.tcl
@@ -4,10 +4,6 @@ bind raw - 352 rpl_whoreply
 bind join - * who_onjoin
 bind nkch - * who_nickchange
 
-proc rpl_whoreply {from cmd text} {
-	putlog "whoreply called: $from $cmd $text"
-}
-
 proc who_onjoin {nick uhost hand chan} {
 	putserv "WHO $nick"
 }
@@ -17,3 +13,8 @@ proc who_nickchange {nick uhost hand chan newnick} {
 	# do utimer command
 	putserv "WHO $newnick"
 }
+
+proc rpl_whoreply {from cmd text} {
+	putlog "whoreply called; from: $from cmd: $cmd text: $text"
+}
+
