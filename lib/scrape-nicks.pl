2c910cf62e9f8996eaa6a505733da2c1a35d2758
molo1134 <molo1134@gmx.com>
2018-11-03 22:23:21 -0400

    scrape

--- lib/scrape-nicks.pl
+++ lib/scrape-nicks.pl
@@ -14,7 +14,7 @@ binmode(STDOUT, ":utf8");
 use Cwd 'realpath';
 use File::Basename;
 
-my @subreddits = ("amateurradio", "hamradio", "rtlsdr");
+my @subreddits = ("hamfest", "hamradio", "rtlsdr", "amateurradio");
 
 #my $useragent = "Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0";
 my $useragent = "foo";
