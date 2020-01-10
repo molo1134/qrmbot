bd78323217b7c87b0df288f4df2609768fd6d93a
molo1134 <molo1134@gmx.com>
2020-01-10 11:03:50 -0500

    log, not channel output

--- scripts/fun.tcl
+++ scripts/fun.tcl
@@ -206,11 +206,11 @@ proc chars2hexlist {string} {
 proc translate { nick host hand chan text } {
 	global translatebin
 
-	putchan $chan [chars2hexlist ${text}]
+	putlog [chars2hexlist ${text}]
 	set query [sanitize_string [string trim ${text}]]
-	putchan $chan [chars2hexlist ${query}]
+	putlog [chars2hexlist ${query}]
 	set query [encoding convertto utf-8 "${query}"]
-	putchan $chan [chars2hexlist ${query}]
+	putlog [chars2hexlist ${query}]
 
 	putlog "translate pub: $nick $host $hand $chan $query"
 
