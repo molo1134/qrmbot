40e3fc225db4513246da43eb609b3c293f59774b
molo1134 <molo1134@gmx.com>
2020-07-05 12:18:23 -0400

    scrape

--- lib/scrape-nicks.pl
+++ lib/scrape-nicks.pl
@@ -102,7 +102,9 @@ our @blacklist = (
   "N1A", "NC45L", "QE37QULU", "SR65K", "T0NITO", "T109J", "T1TWAN", "UP10AD",
   "V3NGE", "G1LV", "XS400CAFE", "O3GEN", "T60R", "DJ027X", "4TR5V", "J3DUDE",
   "LB7GUY", "RC5K", "W33T", "J42J", "D4RKS", "5H15H", "CB77EF", "1MC6FR",
-  "5C4359D", "5RS24Q", "SC0TTP",
+  "5C4359D", "5RS24Q", "SC0TTP", "BD536HP", "BG25LAM", "C4FM", "D710G",
+  "DA5ID", "EN54KI", "F4TE", "FN2X", "FT60R", "J9WILL", "L18CP", "R43SHAH",
+  "R5A", "U3DW", "V25D", "Z0A",
   # opt-out
   "N4ADK");
 
