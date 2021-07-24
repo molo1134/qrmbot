afe0a209b0285cd8b9533e36b715d31e505152ee
molo1134 <molo1134@gmx.com>
2021-07-23 22:05:49 -0400

    nickserv.tcl: fix binds; add logs

--- scripts/nickserv.tcl
+++ scripts/nickserv.tcl
@@ -10,13 +10,16 @@ proc evnt:init_server {type} {
   #global botnick
   #putquick "MODE $botnick +iR-ws"
   global nickservpassword
+  putlog "sending password to nickserv..."
   putquick "PRIVMSG nickserv :identify ${nickservpassword}"
 }
 
 # if a notice is received, identify to keep the nick
-bind notc "*This nickname is registered and protected.*" identify
+bind notc - "*This nickname is registered and protected.*" identify
+bind notc - "This nickname is registered and protected*" identify
 
 proc identify {nick uhost hand text dest} {
   global nickservpassword
+  putlog "sending password to nickserv..."
   putquick "PRIVMSG nickserv :identify ${nickservpassword}"
 }
