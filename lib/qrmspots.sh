1b8f3151624c05bb0811162390fbc1bd557ff49b
molo1134 <molo1134@gmx.com>
2024-06-12 19:25:41 -0400

    add qrmspots.sh to dump recent qrm spots from sqlite db

---
+++ lib/qrmspots.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+SPOTFILE="$HOME/.qrmbot/db/spots.sqlite"
+QUERY="
+    ATTACH DATABASE '$SPOTFILE' AS spots;
+    SELECT DATETIME(timestamp, 'unixepoch') AS time, de, channel, callsign, freq_hz, notes
+    FROM spots.t_spots
+    ORDER BY timestamp DESC
+    LIMIT 30;
+"
+
+if [ ! -e "$SPOTFILE" ]; then
+  echo "missing $SPOTFILE"
+  exit 1
+fi
+
+sqlite3 -readonly -header -column "$SPOTFILE" "$QUERY";
+
