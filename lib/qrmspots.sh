feacb8bfc98f1ba2ec07cd1ac34084a349b1b422
molo1134 <molo1134@gmx.com>
2024-07-03 12:57:24 -0400

    qrmspots.sh: add some informative queries in comments

--- lib/qrmspots.sh
+++ lib/qrmspots.sh
@@ -17,3 +17,16 @@ fi
 #sqlite3 -readonly -header -column "$SPOTFILE" "$QUERY";
 sqlite3 -header -column "$SPOTFILE" "$QUERY";
 
+
+# most spots reported by user:
+# select de, count(*) from spots.t_spots group by de order by 2 desc;
+
+# most commonly spotted callsign
+# select callsign, count(*) from spots.t_spots group by callsign order by 2 desc limit 10;
+
+# number of spots by origin channel
+# select channel, count(*) from spots.t_spots group by channel order by 2 desc;
+
+# spots by band
+# select freq_hz / 1000000 as MHz, count(*) from spots.t_spots group by freq_hz / 1000000;
+
