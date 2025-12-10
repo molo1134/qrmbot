3b8b99498e7bb090214b47aeb8ffe6c251d65715
molo1134 <molo1134@gmx.com>
2025-12-10 15:56:56 -0500

    seen: fix syntax

--- scripts/seen.tcl
+++ scripts/seen.tcl
@@ -171,7 +171,7 @@ proc _seen_output_line { origQuery chan line } {
     set stamp [clock format $msgtime -gmt 1 -format "%Y-%m-%d %H:%M:%S UTC"]
     set now [clock seconds]
     set idle [expr {$now - $tsec} ]
-    set d [expr {int($idle / 86400) ]
+    set d [expr {int($idle / 86400)} ]
     set h [expr {int(($idle % 86400) / 3600)} ]
     set m [expr {int(($idle % 3600) / 60)} ]
 
