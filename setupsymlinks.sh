b04ba0be80709a720b8d5c591f02656e07fc5a4f
molo1134 <molo1134@gmx.com>
2026-02-26 11:05:47 -0500

    setupsymlinks.sh: fix quoting, ln usage

--- setupsymlinks.sh
+++ setupsymlinks.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 #
 # 2-clause BSD license.
-# Copyright (c) 2019 molo1134@github. All rights reserved.
+# Copyright (c) 2019,2026 molo1134@github. All rights reserved.
 
-BASEPATH=$(dirname $0)
-EXECUTABLES=$(find $BASEPATH/lib -type f -perm /111 | grep -v '\.pl$' | sort)
+BASEPATH=$(dirname "$0")
+EXECUTABLES=$(find "$BASEPATH/lib" -type f -perm /111 | grep -v '\.pl$' | sort)
 for e in $EXECUTABLES ; do
 #	echo $e
-	echo ln -s -f $e
-	ln -s -f $e
+	echo ln -s -f "$e" .
+	ln -s -f "$e" .
 done
